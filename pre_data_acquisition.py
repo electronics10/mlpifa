9a68490d5eb24066562fd0dfe77d5d68a2c60e63
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-04-26 00:59:39 +0800

    Update pre_data_acquisition.py

--- pre_data_acquisition.py
+++ pre_data_acquisition.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     os.makedirs("./data/s11", exist_ok=True)
 
     mlpifa = MLPIFA("antennas/mlpifa.cst") # Call mlpifa file
-    mlpifa.set_environment() # set PIFA and blocks for the first call
+    # mlpifa.set_environment() # set PIFA and blocks for the first call
     mlpifa.set_frequency_solver()
     input_data = mlpifa.generate_input() # generate input.csv randomly, [[feedx, binary seq],...]
     iter_start = 0 # start data acquisition from sample 'iter_start'
@@ -23,14 +23,43 @@ for index in range(iter_start, len(input_data)): # Loop through all n samples an
         input_seq = list(input_data[index])
         print(f"\nSample{index} optimizing...")
         # Update feedx and blocks material
-        mlpifa.create_parameters('fx', input_seq[0]) # update fx
+        feedx = input_seq[0]
+        mlpifa.create_parameters('fx', feedx) # update fx
         mlpifa.update_distribution(input_seq[1:]) # ignore fx in the first index and update blocks material
         
+        # Without optimization
+        # Update parameters # post
+        mlpifa.parameters = {"pin_dis":INIT_P1, "patch_len":INIT_P2, "pin_width":INIT_P3} # post
+        for key, value in mlpifa.parameters.items(): # post
+             print(f"key: {key}; value: {value}") # post
+             mlpifa.create_parameters(key, value) # post
+        # Without optimization # post
+        mlpifa.set_port() # post
+        mlpifa.start_simulate() # post
+        print(f"Sample{index} solved without optimization") #post
+        # Obtain S11 for further inspection # post
+        s11 = mlpifa.read('1D Results\\S-Parameters\\S1,1') # [freq, s11 50+j,...]
+        s11 = np.abs(np.array(s11))
+        s11[:,1] = 20*np.log10(s11[:,1]) # change s11 to dB
+        data = pd.DataFrame(s11[:,:-1], columns=['freq', 's11']) # create a DataFrame
+        data.to_csv(f'data/s11/s{index}.csv', index=False) # save to CSV
+        print(f"S11 saved to 'data/s11/s{index}.csv'")
+        # Store data into data.csv for further inspection # post
+        data1 = input_seq
+        for val in mlpifa.parameters.values(): data1.append(val)
+        with open('data/data_wo.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data1)
+            print("Input and ouput parameters stored in data/data_wo.csv")
+        # Clear legacy for optimization # post
+        mlpifa.delete_results() # post
+        mlpifa.delete_port() # post
+
         # Optimize
         mlpifa.set_port()
         start_time = time.time()
         mlpifa.parameters = {"pin_dis":INIT_P1, "patch_len":INIT_P2, "pin_width":INIT_P3} # altered to make optimizer start with these values
-        mlpifa.optimize() # run CST optimizer
+        mlpifa.optimize(feedx) # run CST optimizer
         end_time = time.time()
         print("optimization time =", end_time-start_time)
         print(f"Sample{index} optimized")
@@ -42,13 +71,13 @@ for index in range(iter_start, len(input_data)): # Loop through all n samples an
             writer = csv.writer(csvfile)
             writer.writerow(data)
             print("Input and ouput parameters stored in data/data.csv")
-        # # Obtain optimized S11 for further inspection
-        # s11 = mlpifa.read('1D Results\\S-Parameters\\S1,1') # [freq, s11 50+j,...]
-        # s11 = np.abs(np.array(s11))
-        # s11[:,1] = 20*np.log10(s11[:,1]) # change s11 to dB
-        # data = pd.DataFrame(s11[:,:-1], columns=['freq', 's11']) # create a DataFrame
-        # data.to_csv(f'data/s11/o{index}.csv', index=False) # save to CSV
-        # print(f"S11 saved to 'data/s11/o{index}.csv'")
+        # Obtain optimized S11 for further inspection
+        s11 = mlpifa.read('1D Results\\S-Parameters\\S1,1') # [freq, s11 50+j,...]
+        s11 = np.abs(np.array(s11))
+        s11[:,1] = 20*np.log10(s11[:,1]) # change s11 to dB
+        data = pd.DataFrame(s11[:,:-1], columns=['freq', 's11']) # create a DataFrame
+        data.to_csv(f'data/s11/o{index}.csv', index=False) # save to CSV
+        print(f"S11 saved to 'data/s11/o{index}.csv'")
         # Clear legacy for next iteration
         mlpifa.delete_results()
         mlpifa.delete_port() # feed postion may change in next iterationeed postion may change in next iteration
