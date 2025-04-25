26dc5344a8574db2d102afa90208ccdefcbf347e
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-04-26 01:37:29 +0800

    Update post_plot_s11.py

--- post_plot_s11.py
+++ post_plot_s11.py
@@ -3,9 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pifa_controller import GOAL, GFMIN, GFMAX
 
-CASE = 0
-RANGE = 1
-
 data_dir = "./data/s11/"
 files = [f for f in os.listdir(data_dir) if f.startswith("s") and f.endswith(".csv")]
 files_cst = [f for f in os.listdir(data_dir) if f.startswith("o") and f.endswith(".csv")]
@@ -13,27 +10,30 @@ files_ml = [f for f in os.listdir(data_dir) if f.startswith("m") and f.endswith(
 
 plt.figure(figsize=(10, 5))
 
-for file in files[CASE:CASE+RANGE]:
-    file_path = os.path.join(data_dir, file)
-    df = pd.read_csv(file_path)  # Assuming headers are present in CSV
-    frequency = df.iloc[:, 0]
-    s11 = df.iloc[:, 1]
-    label = file.replace(".csv", "")
-    plt.plot(frequency, s11, label=label, linestyle=':', color = '#000')
-for file in files_cst[CASE:CASE+RANGE]:
-    file_path = os.path.join(data_dir, file)
-    df = pd.read_csv(file_path)  # Assuming headers are present in CSV
-    frequency = df.iloc[:, 0]
-    s11 = df.iloc[:, 1]
-    label = file.replace(".csv", "")
-    plt.plot(frequency, s11, label=label, linestyle='--', color = '#4D0')
-for file in files_ml[CASE:CASE+RANGE]:
-    file_path = os.path.join(data_dir, file)
-    df = pd.read_csv(file_path)  # Assuming headers are present in CSV
-    frequency = df.iloc[:, 0]
-    s11 = df.iloc[:, 1]
-    label = file.replace(".csv", "")
-    plt.plot(frequency, s11, label=label, linestyle='-', color = '#00F')
+def parse(case, ran):
+    for file in files[case:case+ran]:
+        file_path = os.path.join(data_dir, file)
+        df = pd.read_csv(file_path)  # Assuming headers are present in CSV
+        frequency = df.iloc[:, 0]
+        s11 = df.iloc[:, 1]
+        label = file.replace(".csv", "")
+        plt.plot(frequency, s11, label=label, linestyle=':', color = '#000')
+    for file in files_cst[case:case+ran]:
+        file_path = os.path.join(data_dir, file)
+        df = pd.read_csv(file_path)  # Assuming headers are present in CSV
+        frequency = df.iloc[:, 0]
+        s11 = df.iloc[:, 1]
+        label = file.replace(".csv", "")
+        plt.plot(frequency, s11, label=label, linestyle='--', color = '#4D0')
+    for file in files_ml[case:case+ran]:
+        file_path = os.path.join(data_dir, file)
+        df = pd.read_csv(file_path)  # Assuming headers are present in CSV
+        frequency = df.iloc[:, 0]
+        s11 = df.iloc[:, 1]
+        label = file.replace(".csv", "")
+        plt.plot(frequency, s11, label=label, linestyle='-', color = '#00F')
+
+for case in range(len(files)): parse(case, 1)
 
 plt.axvline(x = GFMIN, linestyle='-.', color = '#999')
 plt.axvline(x = GFMAX, linestyle='-.', color = '#999')
