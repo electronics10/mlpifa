f321d9c3d476451e3cfe2e831f781c9e4bb57c99
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-04-26 01:17:25 +0800

    Update post_plot_s11.py

--- post_plot_s11.py
+++ post_plot_s11.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from pifa_controller import GOAL, GFMIN, GFMAX
 
 CASE = 0
-RANGE = -1
+RANGE = 1
 
 data_dir = "./data/s11/"
 files = [f for f in os.listdir(data_dir) if f.startswith("s") and f.endswith(".csv")]
@@ -13,14 +13,14 @@ files_ml = [f for f in os.listdir(data_dir) if f.startswith("m") and f.endswith(
 
 plt.figure(figsize=(10, 5))
 
-for file in files[CASE:]:
+for file in files[CASE:CASE+RANGE]:
     file_path = os.path.join(data_dir, file)
     df = pd.read_csv(file_path)  # Assuming headers are present in CSV
     frequency = df.iloc[:, 0]
     s11 = df.iloc[:, 1]
     label = file.replace(".csv", "")
     plt.plot(frequency, s11, label=label, linestyle=':', color = '#000')
-for file in files_cst[CASE:]:
+for file in files_cst[CASE:CASE+RANGE]:
     file_path = os.path.join(data_dir, file)
     df = pd.read_csv(file_path)  # Assuming headers are present in CSV
     frequency = df.iloc[:, 0]
