59885105e5c0d2aaa8db440156681f3b63c847f7
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-04-30 09:35:01 +0800

    Update pifa_controller.py

--- pifa_controller.py
+++ pifa_controller.py
@@ -6,31 +6,7 @@ import cst.interface as csti
 import os
 import numpy as np
 import pandas as pd
- 
-N_SAMPLES = 500
-SEED = 42
-
-BLOCKS_NUM = 9
-BLOCK_LEN = 3 # mm
-BLOCK_SEPERATION = 2
-FEEDX_MIN = 7 # mm # initial feedx at mid
-FEEDX_MAX = 10 # mm # initial feedx at mid
-REGIONX = 21 # mm
-REGIONY = 8 # mm
-OFFSETY = 150 - BLOCK_LEN - REGIONY # mm
-PATCH_OFFSET = 0.1 # mm
-
-INIT_P1 = 3.87 # mm, pin dis
-INIT_P2 = 20 # mm, patch len
-INIT_P3 = 6.34 # mm, pin width
-LINE = 1 # mm
-
-FMIN = 2 # GHz
-FMAX = 3 # GHz
-
-GOAL = -10 # -7 # dB
-GFMIN = 2.4 # GHz
-GFMAX = 2.5 # GHz
+from settings import*
 
 class CSTInterface:
     def __init__(self, fname):
