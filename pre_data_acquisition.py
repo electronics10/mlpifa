1c1f800e9334a241108546b35770a2bea62690c4
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-05-02 14:15:21 +0800

    Update pre_data_acquisition.py

--- pre_data_acquisition.py
+++ pre_data_acquisition.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     mlpifa = MLPIFA("antennas/mlpifa.cst") # Call mlpifa file
     # mlpifa.set_environment() # set PIFA and blocks for the first call
     mlpifa.set_frequency_solver()
-    input_data = mlpifa.generate_input(5) # generate input.csv randomly, [[feedx, binary seq],...]
-    iter_start = 0 # start data acquisition from sample 'iter_start'
+    input_data = mlpifa.generate_input() # generate input.csv randomly, [[feedx, binary seq],...]
+    iter_start = 5 # start data acquisition from sample 'iter_start'
 
 for index in range(iter_start, len(input_data)): # Loop through all n samples and run CST optimizer
         mlpifa.delete_results() # clear legacy
@@ -44,12 +44,12 @@ for index in range(iter_start, len(input_data)): # Loop through all n samples an
             writer = csv.writer(csvfile)
             writer.writerow(data)
             print("Input and ouput parameters stored in data/data.csv")
-        # Obtain optimized S11 for further inspection
-        s11 = mlpifa.read('1D Results\\S-Parameters\\S1,1') # [freq, s11 50+j,...]
-        s11 = np.abs(np.array(s11))
-        s11[:,1] = 20*np.log10(s11[:,1]) # change s11 to dB
-        data = pd.DataFrame(s11[:,:-1], columns=['freq', 's11']) # create a DataFrame
-        data.to_csv(f'data/s11/o{index}.csv', index=False) # save to CSV
+        # # Obtain optimized S11 for further inspection
+        # s11 = mlpifa.read('1D Results\\S-Parameters\\S1,1') # [freq, s11 50+j,...]
+        # s11 = np.abs(np.array(s11))
+        # s11[:,1] = 20*np.log10(s11[:,1]) # change s11 to dB
+        # data = pd.DataFrame(s11[:,:-1], columns=['freq', 's11']) # create a DataFrame
+        # data.to_csv(f'data/s11/o{index}.csv', index=False) # save to CSV
         print(f"S11 saved to 'data/s11/o{index}.csv'")
         # Clear legacy for next iteration
         mlpifa.delete_results()
