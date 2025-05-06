a484ad6400507a89744eebc646dc01bc3eaa4f30
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-05-06 11:31:11 +0800

    Create post_check.py

---
+++ post_check.py
@@ -0,0 +1,17 @@
+from settings import BLOCKS_NUM, REGIONX, REGIONY
+import pandas as pd
+
+
+filePath = 'data/post_prediction.csv'
+df = pd.read_csv(filePath, header=None)
+
+for case in range(len(df)):
+    fx = df.iloc[case, 0]
+    Lmax = REGIONX - fx - 0.1
+    Dmax = fx - 0.1
+    Hmax = REGIONY - 0.1
+    if df.iloc[case, BLOCKS_NUM+1] > Lmax: df.iloc[case, BLOCKS_NUM+1]=Lmax
+    if df.iloc[case, BLOCKS_NUM+2] > Dmax: df.iloc[case, BLOCKS_NUM+2]=Dmax
+    if df.iloc[case, BLOCKS_NUM+3] > Hmax: df.iloc[case, BLOCKS_NUM+3]=Hmax
+
+df.to_csv(filePath, index=False, header=None)
