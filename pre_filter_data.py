6834fc3ab8b67fdaf16bd316bbb68546319b4d09
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-05-09 16:58:29 +0800

    Update pre_filter_data.py

--- pre_filter_data.py
+++ pre_filter_data.py
@@ -1,17 +1,37 @@
 from settings import BLOCKS_NUM, REGIONX, REGIONY
 import pandas as pd
+import numpy as np
+import csv
 
+LFREQ = 2.4 # GHz
+HFREQ = 2.5 # GHz
+GOAL = -6 # dB
+BOUND = -1 # dB
 
-filePath = 'data/post_prediction.csv'
-df = pd.read_csv(filePath, header=None)
+df = pd.read_csv("data/data.csv", header=None)
+output_file = "data/data_filtered.csv"
 
-for case in range(len(df)):
-    fx = df.iloc[case, 0]
-    Lmax = REGIONX - fx - 0.1
-    Dmax = fx - 0.1
-    Hmax = REGIONY - 0.1
-    if df.iloc[case, BLOCKS_NUM+1] > Lmax: df.iloc[case, BLOCKS_NUM+1]=Lmax
-    if df.iloc[case, BLOCKS_NUM+2] > Dmax: df.iloc[case, BLOCKS_NUM+2]=Dmax
-    if df.iloc[case, BLOCKS_NUM+3] > Hmax: df.iloc[case, BLOCKS_NUM+3]=Hmax
-
-df.to_csv(filePath, index=False, header=None)
+for index in range(5):#len(df)):
+    # Read s11_{index}
+    df_s11 = pd.read_csv(f"data/s11/s11_{index}.csv")
+    j = 0
+    quality = 0
+    while True:
+        # Read fequency
+        freq = df_s11.iloc[j, 0]
+        if freq >= LFREQ:
+            # Read s11
+            s11 = df_s11.iloc[j, 1]
+            # Record quality
+            quality += GOAL - s11
+        if freq >= HFREQ: break
+        j += 1
+    normalizer_quality = quality/j
+    if normalizer_quality < BOUND: pass
+    else:
+        with open(output_file, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(df.iloc[index, :])
+        with open('data/output_index.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([index])   
