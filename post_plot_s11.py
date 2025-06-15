1aa4d4e40a7c96519ada3325a047efd29451a037
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-06-15 20:24:11 +0800

    Update post_plot_s11.py

--- post_plot_s11.py
+++ post_plot_s11.py
@@ -6,10 +6,12 @@ from settings import GOAL, GFMIN, GFMAX
 GOAL = -6
 
 data_dir = "./data/comparison/"
-files = [f for f in os.listdir(data_dir) if f.startswith("i") and f.endswith(".csv")]
-files_cst = [f for f in os.listdir(data_dir) if f.startswith("o") and f.endswith(".csv")]
-files_ml = [f for f in os.listdir(data_dir) if f.startswith("m") and f.endswith(".csv")]
-files_ml_weighted = [f for f in os.listdir(data_dir) if f.startswith("w") and f.endswith(".csv")]
+files = [f for f in os.listdir(data_dir) if f.startswith("initial") and f.endswith(".csv")]
+files_cst = [f for f in os.listdir(data_dir) if f.startswith("CST") and f.endswith(".csv")]
+files_FNN = [f for f in os.listdir(data_dir) if f.startswith("FNN") and f.endswith(".csv")]
+files_RNN = [f for f in os.listdir(data_dir) if f.startswith("RNN") and f.endswith(".csv")]
+files_TAB = [f for f in os.listdir(data_dir) if f.startswith("TAB") and f.endswith(".csv")]
+files_FNN_weighted = [f for f in os.listdir(data_dir) if f.startswith("weighted") and f.endswith(".csv")]
 
 def parse(case, ran):
     plt.figure(figsize=(10, 5))
@@ -19,32 +21,50 @@ def parse(case, ran):
         frequency = df.iloc[:, 0]
         s11 = df.iloc[:, 1]
         plt.plot(frequency, s11, label='Initial', linestyle=':', color = '#000')
+        
     for file in files_cst[case:case+ran]:
         file_path = os.path.join(data_dir, file)
         df = pd.read_csv(file_path)  # Assuming headers are present in CSV
         frequency = df.iloc[:, 0]
         s11 = df.iloc[:, 1]
-        plt.plot(frequency, s11, label='CST', linestyle='--', color = '#4D0')
-    for file in files_ml[case:case+ran]:
+        plt.plot(frequency, s11, label='CST Optimizer', linestyle='--', color = '#4D0')
+        
+    for file in files_FNN[case:case+ran]:
         file_path = os.path.join(data_dir, file)
         df = pd.read_csv(file_path)  # Assuming headers are present in CSV
         frequency = df.iloc[:, 0]
         s11 = df.iloc[:, 1]
-        plt.plot(frequency, s11, label='Our Work', linestyle='-', color = '#00F')
-    for file in files_ml_weighted[case:case+ran]:
+        plt.plot(frequency, s11, label='Our Work (FNN)', linestyle='-', color = '#00F')
+
+    for file in files_FNN_weighted[case:case+ran]:
         file_path = os.path.join(data_dir, file)
         df = pd.read_csv(file_path)  # Assuming headers are present in CSV
         frequency = df.iloc[:, 0]
         s11 = df.iloc[:, 1]
-        plt.plot(frequency, s11, label='Our Work (weighted)', linestyle='-', color = '#F00')
-    plt.axvline(x = GFMIN, linestyle='-.', color = '#999')
-    plt.axvline(x = GFMAX, linestyle='-.', color = '#999')
-    plt.axhline(y = GOAL, linestyle='-.', color = '#999')
+        plt.plot(frequency, s11, label='Our Work (weighted FNN)', linestyle='-:', color = '#808')
+        
+    for file in files_RNN[case:case+ran]:
+        file_path = os.path.join(data_dir, file)
+        df = pd.read_csv(file_path)  # Assuming headers are present in CSV
+        frequency = df.iloc[:, 0]
+        s11 = df.iloc[:, 1]
+        plt.plot(frequency, s11, label='Our Work (RNN)', linestyle='-:', color = '#B00')
+        
+    for file in files_TAB[case:case+ran]:
+        file_path = os.path.join(data_dir, file)
+        df = pd.read_csv(file_path)  # Assuming headers are present in CSV
+        frequency = df.iloc[:, 0]
+        s11 = df.iloc[:, 1]
+        plt.plot(frequency, s11, label='Our Work (TabTransformer)', linestyle='-:', color = '#FA4')
+        
+    # plt.axvline(x = GFMIN, linestyle='-.', color = '#999')
+    # plt.axvline(x = GFMAX, linestyle='-.', color = '#999')
+    # plt.axhline(y = GOAL, linestyle='-.', color = '#999')
     plt.xlabel("Frequency")
     plt.ylabel("S11")
-    plt.title(f"Comparison{case}")
+    plt.title(f"Case{case}")
     plt.legend()
-    plt.grid()
+    # plt.grid()
     plt.savefig(f'data/comparison/comparison{case}.png')
 
 for case in range(len(files)): parse(case, 1)
